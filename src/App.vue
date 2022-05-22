3c2fbe02b7a1d529c4161134304e9a46b52c64e2
gitEllE_if <elvira_lina@mail.ru>
2022-05-22 17:17:48 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-app-bar app></v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
